9883643c6abe8c35b37e5b89cbb1fc4b9aff6005
jithingp <jithingpanoli18@gmail.com>
2022-06-06 18:43:02 +0530

    jumpto method defined

--- src/index.js
+++ src/index.js
@@ -53,13 +53,14 @@ import './index.css';
               squares : Array(9).fill(null),
             }
           ],
+          stepNumber: 0,
           xIsNext: true,
         }
     }
     handleClick(i){
       const history = this.state.history
       const current = history[history.length-1]
-      const squares = this.state.squares.slice()
+      const squares = current.squares.slice()
       if(calculateWinner(squares)||squares[i]){
         return
       }
@@ -71,10 +72,26 @@ import './index.css';
         xIsNext: !this.state.xIsNext,
       })
   }
+
+  jumpTo(step){
+    this.setState({
+      stepNumber: step,
+      xIsNext : (step%2)===0
+    })
+  }
     render() {
       const history = this.state.history
       const current = history[history.length-1]
       const winner = calculateWinner(current.squares)
+
+      const moves = history.map((step,move)=>{
+        const desc = move? 'Go to move #' + move : 'Go to game start'
+        return(
+          <li key={move}>
+            <button onClick={()=> this.jumpTo(move)}>{desc}</button>
+          </li>
+        )
+      })
       let status
       if(winner){
         status='Winner: '+winner
@@ -91,7 +108,7 @@ import './index.css';
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
